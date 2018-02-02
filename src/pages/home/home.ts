c82e48dbcdb7b23f59a042ddaa30b2c21383491f
Fernando Lopez <fer.ragnar19@gmail.com>
2018-02-02 14:55:21 -0300

    agregado http
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,70 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, Loading } from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+//import { MapaDetallePage } from '../mapa-detalle/mapa-detalle';
+//import { TodoMapaPage } from '../todo-mapa/todo-mapa';
+
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
-export class HomePage {
+export class HomePage{
+  museumList = [];
+  loader: Loading;
+  filteredMuseum = [];
+  isfiltered: boolean ;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private http:Http, public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController) {
+    this.isfiltered = false;
+    this.presentLoading();
+    this.http.get('https://demo-fernando.herokuapp.com/todos')
+    .map(res => res.json())
+    .subscribe(data => {
+        this.museumList = data
+        this.loader.dismiss();
+      },
+      err => console.log("error is "+err), // error
+      () => console.log('data de telos completa'+ this.museumList) // complete
+    );
+  }
 
+  presentLoading() {
+    this.loader = this.loadingCtrl.create({
+      content: "Cargando..."
+    });
+    this.loader.present();
+  }
+
+  itemTapped(museum) {
+     this.navCtrl.push(MapaDetallePage, {
+       museum: museum
+     });
+  }
+
+  allMuseumMap(){
+    this.navCtrl.push(TodoMapaPage, {
+       museumList: this.museumList
+    });
+  }
+
+  doRefresh(refresher) {
+    this.http.get('https://demo-fernando.herokuapp.com/todos')
+    .map(res => res.json())
+    .subscribe(data => {
+        this.museumList = data
+        this.loader.dismiss();
+      },
+      err => console.log("error is "+err), // error
+      () => console.log('data de telos completa'+ this.museumList) // complete
+    );
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
   }
 
 }
+
